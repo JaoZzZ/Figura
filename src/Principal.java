b2bd60b56e21acac28bfbb2cb5ab3423dace6cda
Aluno IFSC <IFSC@CDR-770640.cacador.ifsc.edu.br>
2021-12-14 16:23:28 -0300

    Figura

---
+++ src/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IFSC
+ */
+public class Principal {
+    public static void main(String[] args){
+    Circulo cir = new Circulo();
+    cir.desenhar();
+    
+    Quadrado qua = new Quadrado();
+    qua.desenhar();
+    
+    Triangulo tri = new Triangulo();
+    tri.desenhar();
+    
+   }
+}
